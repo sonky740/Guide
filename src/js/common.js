dbd24f184d5036e103fa95fb8764ad8514dcc8a6
sonky740 <mykiller4@naver.com>
2021-07-23 13:50:43 +0900

    javascript 변수 수정

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   UI_Control.layout();
-  UI_Control.url();
   UI_Control.checkAll();
   UI_Control.tab();
   UI_Control.contextMenu();
@@ -17,12 +16,12 @@ var UI_Control = {
    * layout
    */
   layout: function () {
-    var $url = window.location.href.split('/');
-    var $urlLast = $url[$url.length - 1];
+    const $url = window.location.href.split('/');
+    const $urlLast = $url[$url.length - 1];
 
     // lnb
-    var $lnbParent = document.querySelector('.guide-nav');
-    var $lnb = '<strong>Guide</strong>'
+    const $lnbParent = document.querySelector('.guide-nav');
+    let $lnb = '<strong>Guide</strong>'
     $lnb += '<ul>'
     $lnb += '<li><a href="/Guide/src/html/guide/accordion.html">accordion</a></li>'
     $lnb += '<li><a href="/Guide/src/html/guide/context-menu.html">context-menu</a></li>'
@@ -32,17 +31,17 @@ var UI_Control = {
     $lnbParent.innerHTML = $lnb;
 
     // lnb === url ? active : null
-    var $lnbTrigger = document.querySelectorAll('.guide-nav ul li a');
+    const $lnbTrigger = document.querySelectorAll('.guide-nav ul li a');
     [].forEach.call($lnbTrigger, function (el) {
-      var $target = el.getAttribute('href').split('/');
-      var $targetLast = $target[$target.length - 1];
+      const $target = el.getAttribute('href').split('/');
+      const $targetLast = $target[$target.length - 1];
 
       if ($urlLast === $targetLast) el.classList.add('active');
     })
 
     // header 
-    var $headerParent = document.querySelector('.header');
-    var $header = '<div>'
+    const $headerParent = document.querySelector('.header');
+    let $header = '<div>'
     $header += '<h1>'
     $header += '<a href="/Guide/src/html/" title="홈으로">Sonky</a>'
     $header += '</h1>'
@@ -51,32 +50,13 @@ var UI_Control = {
   },
 
   /**
-   * url에 따른 컨텐츠
-   */
-  url: function () {
-    var $url = window.location.href.split('?')[1];
-    if ($url) {
-      var $urlCode = $url.slice(0, 6);
-    }
-
-    var $dataUrl = document.querySelectorAll('[data-url]');
-    [].forEach.call($dataUrl, function (el) {
-      var $elValue = el.getAttribute('data-url');
-
-      if ($urlCode !== $elValue) {
-        el.parentNode.removeChild(el)
-      }
-    })
-  },
-
-  /**
    * 체크박스
    */
   checkAll: function () {
-    var $check = document.querySelectorAll('input[data-checkbox]');
+    const $check = document.querySelectorAll('input[data-checkbox]');
     [].forEach.call($check, function (el) {
-      var $elem = '[name=' + el.getAttribute('data-checkbox') + ']:not([data-checkbox])';
-      var $bullet = document.querySelectorAll($elem);
+      const $elem = '[name=' + el.getAttribute('data-checkbox') + ']:not([data-checkbox])';
+      const $bullet = document.querySelectorAll($elem);
 
       [].forEach.call($bullet, function (al) {
         // 전체 클릭, 해제
@@ -90,7 +70,7 @@ var UI_Control = {
 
         // 요소 클릭, 해제
         al.addEventListener('click', function () {
-          var $checked = document.querySelectorAll('input:checked' + $elem).length;
+          const $checked = document.querySelectorAll('input:checked' + $elem).length;
           if ($checked === $bullet.length) {
             el.checked = true;
           } else {
@@ -104,7 +84,7 @@ var UI_Control = {
         }
 
         // 전부 클릭이 되어있다면
-        var $gChecked = document.querySelectorAll('input:checked' + $elem).length;
+        const $gChecked = document.querySelectorAll('input:checked' + $elem).length;
         if ($gChecked === $bullet.length) {
           el.checked = true;
         }
@@ -116,14 +96,14 @@ var UI_Control = {
    * 탭
    */
   tab: function () {
-    var $tab = document.querySelectorAll('[data-tab-group]');
+    const $tab = document.querySelectorAll('[data-tab-group]');
     [].forEach.call($tab, function (el) {
-      var $group = el.getAttribute('data-tab-group');
-      var $trigger = el.querySelectorAll('[data-tabtit]');
-      var $bullet = document.querySelectorAll('[data-tabcon]');
+      const $group = el.getAttribute('data-tab-group');
+      const $trigger = el.querySelectorAll('[data-tabtit]');
+      const $bullet = document.querySelectorAll('[data-tabcon]');
 
       [].forEach.call($trigger, function (el2) {
-        var $title = el2.getAttribute('data-tabtit')
+        const $title = el2.getAttribute('data-tabtit')
         el2.addEventListener('click', function (e) {
           // a태그 일 경우
           if (el2.getAttribute('href')) {
@@ -136,8 +116,8 @@ var UI_Control = {
           }
 
           [].forEach.call($bullet, function (el3) {
-            var $content = el3.getAttribute('data-tabcon');
-            var $content2 = el3.getAttribute('id');
+            const $content = el3.getAttribute('data-tabcon');
+            const $content2 = el3.getAttribute('id');
 
             if ($group === $content) {
               if ($title === $content2) {
@@ -156,10 +136,10 @@ var UI_Control = {
    * contextMenu
    */
   contextMenu: function () {
-    var items = document.querySelectorAll('[data-context] > button');
+    const items = document.querySelectorAll('[data-context] > button');
 
     document.body.addEventListener('click', function (e) {
-      var targetClassList = e.target;
+      const targetClassList = e.target;
       if (targetClassList.classList.contains('context')) return;
 
       // 개별토글
@@ -186,17 +166,17 @@ var UI_Control = {
    * 아코디언
    */
   accr: function () {
-    var $accr = document.querySelectorAll('[data-toggle="accr"]');
+    const $accr = document.querySelectorAll('[data-toggle="accr"]');
 
     Array.prototype.forEach.call($accr, function ($accrEl) {
-      var $body = $accrEl.querySelectorAll('[data-accr]');
+      const $body = $accrEl.querySelectorAll('[data-accr]');
 
       Array.prototype.forEach.call($body, function ($bodyEl) {
-        var $trigger = $bodyEl.querySelector('[data-accr-trigger]');
-        var $triggerAll = $accrEl.querySelectorAll('[data-accr-trigger]'); // 이벤트 진행 중에 트리거의 이벤트를 없애기 위함.
-        var $trigger_ir = $trigger.querySelector('.blind');
-        var $target = $bodyEl.querySelector('[data-accr-target]');
-        var $target_body = $target.querySelector('[data-accr-target] .accr_content_body');
+        const $trigger = $bodyEl.querySelector('[data-accr-trigger]');
+        const $triggerAll = $accrEl.querySelectorAll('[data-accr-trigger]'); // 이벤트 진행 중에 트리거의 이벤트를 없애기 위함.
+        const $trigger_ir = $trigger.querySelector('.blind');
+        const $target = $bodyEl.querySelector('[data-accr-target]');
+        const $target_body = $target.querySelector('[data-accr-target] .accr_content_body');
 
         // init
         if ($bodyEl.getAttribute('data-accr') === 'show') {
@@ -243,7 +223,7 @@ var UI_Control = {
             $target.style.height = $target_body.clientHeight + 'px';
 
             // 클릭 이벤트를 없애기 위함.
-            var stopFunc = function (e) {
+            const stopFunc = function (e) {
               e.preventDefault();
               e.stopPropagation();
               e.stopImmediatePropagation();
