f795c34bf29b99a94fa0be0ea9fc21dcec37d21e
sonky740 <mykiller4@naver.com>
2021-09-27 08:33:28 +0900

    jquery 아코디언 개선

--- src/js/common_jquery.js
+++ src/js/common_jquery.js
@@ -12,13 +12,15 @@ UI.tab = {
         this.click();
     },
     createSelector: function () {
-        this.$tab = UI.$body.find(".tabs");
-        this.$tabTrigger = this.$tab.find("[data-tab-trigger]");
-        this.$tabTarget = this.$tab.find(".tabs-content");
+        this.$tab = UI.$body.find("[data-tabs]");
+        this.$tabTrigger = this.$tab.find("[data-tabs-trigger]");
+        this.$tabTarget = this.$tab.find("[data-tabs-target]");
         this.isTransitioning = false;
     },
     click: function () {
-
+        this.$tabTrigger.off('click.tab').on('click.tab', function () {
+            console.log('a')
+        })
     },
     transition: function () {
 
@@ -35,10 +37,11 @@ UI.accor = {
 
             // 펼쳐져 있을 경우
             if ($(this).hasClass('on')) {
-                $(this).find('>.accordion-title .blind').text('접기');
+                $(this).find('[data-accor-trigger]').addClass('on');
+                $(this).find('[data-accor-trigger] .blind').text('접기');
                 initTarget.addClass('shown');
             } else {
-                $(this).find('>.accordion-title .blind').text('펼치기');
+                $(this).find('[data-accor-trigger] .blind').text('펼치기');
                 initTarget.addClass('hidden');
             }
         })
@@ -58,10 +61,13 @@ UI.accor = {
             // 각각 활성화
             if (item.hasClass('on')) {
                 target.removeClass('shown');
+                $(this).find('.blind').text('펼치기');
+
                 /* 애니메이션 없을 시 */
                 if ($(this).closest('[data-accor-animation="false"]').length) {
                     target.addClass('hidden');
                     item.removeClass('on');
+                    $(this).removeClass('on');
                     target.trigger('accor.hidden');
                     return false;
                 }
@@ -70,18 +76,6 @@ UI.accor = {
                 target.css('height', target.children().outerHeight());
                 target.addClass('hiding');
                 target.css('height', 0);
-
-                target.off('transitionstart').on('transitionstart', function () {
-                    $(this).trigger('accor.hiding')
-                })
-
-                target.off('transitionend').on('transitionend', function () {
-                    $(this).removeClass('hiding');
-                    $(this).addClass('hidden');
-                    $(this).removeAttr('style');
-
-                    $(this).trigger('accor.hidden');
-                })
             } else {
                 // 하나만 활성화일 때
                 if ($(this).closest('[data-accor]').data('accor') === "only") {
@@ -89,62 +83,50 @@ UI.accor = {
                         const targetAll = $(this).find('>[data-accor-target]');
 
                         if (targetAll.hasClass('shown')) {
-                            $(this).removeClass('on');
+                            $(this).find('[data-accor-trigger] .blind').text('펼치기');
+                            $(this).find('[data-accor-trigger]').removeClass('on');
+
                             /* 애니메이션 없을 시 */
                             if ($(this).closest('[data-accor-animation="false"]').length) {
                                 targetAll.removeClass('shown');
                                 targetAll.addClass('hidden');
+                                $(this).removeClass('on');
+
                                 targetAll.trigger('accor.hidden');
                                 return false;
                             }
                             /* // 애니메이션 없을 시 */
                             targetAll.css('height', targetAll.children().outerHeight());
                             targetAll.css('height', targetAll.children().outerHeight());
+
                             targetAll.removeClass('shown');
                             targetAll.addClass('hiding');
                             targetAll.css('height', 0);
 
-                            targetAll.off('transitionstart').on('transitionstart', function () {
-                                $(this).trigger('accor.hiding');
-                            })
-
-                            targetAll.off('transitionend').on('transitionend', function () {
-                                $(this).removeClass('hiding');
-                                $(this).addClass('hidden');
-                                $(this).removeAttr('style');
-
-                                $(this).trigger('accor.hidden');
-                            })
+                            UI.accor.transition(targetAll);
                         }
                     })
                 }
-
                 target.removeClass('hidden');
+                $(this).find('.blind').text('접기');
                 /* 애니메이션 없을 시 */
                 if ($(this).closest('[data-accor-animation="false"]').length) {
                     target.addClass('shown');
                     item.addClass('on');
+                    $(this).addClass('on');
+
                     target.trigger('accor.shown');
                     return false;
                 }
                 /* // 애니메이션 없을 시 */
                 target.addClass('showing');
                 target.css('height', target.children().outerHeight());
-
-                target.off('transitionstart').on('transitionstart', function () {
-                    $(this).trigger('accor.showing')
-                })
-
-                target.off('transitionend').on('transitionend', function () {
-                    $(this).removeClass('showing');
-                    $(this).addClass('shown');
-                    $(this).removeAttr('style');
-
-                    $(this).trigger('accor.shown');
-                })
             }
 
-            item.toggleClass('on');
+            item.addClass('on');
+            $(this).toggleClass('on');
+
+            UI.accor.transition(target);
 
             // 이벤트 중에 클릭 이벤트 취소
             UI.accor.$accorTrigger.off('click.accor', UI.accor.click());
@@ -154,6 +136,32 @@ UI.accor = {
                 target.off('transitionstart').off('transitionend');
             })
         });
+    },
+    transition: function (target) {
+        target.off('transitionstart.accor').on('transitionstart.accor', function () {
+            if (target.hasClass('showing')) {
+                target.trigger('accor.showing')
+            } else if (target.hasClass('hiding')) {
+                target.trigger('accor.hiding')
+            }
+        })
+        target.off('transitionend.accor').on('transitionend.accor', function () {
+            if (target.hasClass('showing')) {
+                target.removeClass('showing');
+                target.addClass('shown');
+                target.removeAttr('style');
+
+                target.trigger('accor.shown');
+            } else if (target.hasClass('hiding')) {
+                target.removeClass('hiding');
+                target.addClass('hidden');
+                target.removeAttr('style');
+
+                target.closest('[data-accor-item]').removeClass('on');
+
+                target.trigger('accor.hidden');
+            }
+        })
     }
 };
 
@@ -261,6 +269,7 @@ $(function () {
     UI.$window = $(window);
     UI.$body = $("body");
     if (UI.hasJqueryObject(UI.$body.find(".tab-wrap"))) UI.tab.init();
+    if (UI.hasJqueryObject(UI.$body.find('[data-tabs-trigger]'))) UI.tab.init();
     if (UI.hasJqueryObject(UI.$body.find('[data-accor]'))) UI.accor.init();
     if (UI.hasJqueryObject(UI.$body.find('[data-tooltip-trigger]'))) UI.tooltip.init();
 });
