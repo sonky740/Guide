97b7b49f75fd0e2534cb6d7f1e57041593623f1b
sonky740 <mykiller4@naver.com>
2021-09-27 16:48:03 +0900

    swiper custom

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,7 @@ function numberComma(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-// siblings
+// 형제 요소
 function siblings(node) {
   var children = node.parentElement.children;
   var tempArr = [];
@@ -731,6 +731,144 @@ UI_Control.Tooltip = {
   }
 }
 
+UI_Control.swiper = {
+  init: function () {
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+    document.addEventListener('touchend', handleTouchEnd, false);
+
+    var xDown = null;
+    var yDown = null;
+    var xDiff = null;
+    var yDiff = null;
+    var timeDown = null;
+    var startEl = null;
+
+    /**
+     * Fires swiped event if swipe detected on touchend
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchEnd(e) {
+
+      // if the user released on a different target, cancel!
+      if (startEl !== e.target) return;
+
+      var swipeThreshold = parseInt(getNearestAttribute(startEl, 'data-swipe-threshold', '20'), 10); // default 20px
+      var swipeTimeout = parseInt(getNearestAttribute(startEl, 'data-swipe-timeout', '500'), 10); // default 500ms
+      var timeDiff = Date.now() - timeDown;
+      var eventType = '';
+      var changedTouches = e.changedTouches || e.touches || [];
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) { // most significant
+        if (Math.abs(xDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+          if (xDiff > 0) {
+            eventType = 'swiped-left';
+          } else {
+            eventType = 'swiped-right';
+          }
+        }
+      } else if (Math.abs(yDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+        if (yDiff > 0) {
+          eventType = 'swiped-up';
+        } else {
+          eventType = 'swiped-down';
+        }
+      }
+
+      if (eventType !== '') {
+
+        var eventData = {
+          dir: eventType.replace(/swiped-/, ''),
+          xStart: parseInt(xDown, 10),
+          xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
+          yStart: parseInt(yDown, 10),
+          yEnd: parseInt((changedTouches[0] || {}).clientY || -1, 10)
+        };
+
+        // fire `swiped` event event on the element that started the swipe
+        startEl.dispatchEvent(new CustomEvent('swiped', {
+          bubbles: true,
+          cancelable: true,
+          detail: eventData
+        }));
+
+        // fire `swiped-dir` event on the element that started the swipe
+        startEl.dispatchEvent(new CustomEvent(eventType, {
+          bubbles: true,
+          cancelable: true,
+          detail: eventData
+        }));
+      }
+
+      // reset values
+      xDown = null;
+      yDown = null;
+      timeDown = null;
+    }
+
+    /**
+     * Records current location on touchstart event
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchStart(e) {
+
+      // if the element has data-swipe-ignore="true" we stop listening for swipe events
+      if (e.target.getAttribute('data-swipe-ignore') === 'true') return;
+
+      startEl = e.target;
+
+      timeDown = Date.now();
+      xDown = e.touches[0].clientX;
+      yDown = e.touches[0].clientY;
+      xDiff = 0;
+      yDiff = 0;
+    }
+
+    /**
+     * Records location diff in px on touchmove event
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchMove(e) {
+
+      if (!xDown || !yDown) return;
+
+      var xUp = e.touches[0].clientX;
+      var yUp = e.touches[0].clientY;
+
+      xDiff = xDown - xUp;
+      yDiff = yDown - yUp;
+    }
+
+    /**
+     * Gets attribute off HTML element or nearest parent
+     * @param {object} el - HTML element to retrieve attribute from
+     * @param {string} attributeName - name of the attribute
+     * @param {any} defaultValue - default value to return if no match found
+     * @returns {any} attribute value or defaultValue
+     */
+    function getNearestAttribute(el, attributeName, defaultValue) {
+
+      // walk up the dom tree looking for data-action and data-trigger
+      while (el && el !== document.documentElement) {
+
+        var attributeValue = el.getAttribute(attributeName);
+
+        if (attributeValue) {
+          return attributeValue;
+        }
+
+        el = el.parentNode;
+      }
+
+      return defaultValue;
+    }
+  }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
   // IE closest 대응
   if (!Element.prototype.matches) {
@@ -777,4 +915,5 @@ window.addEventListener('DOMContentLoaded', function () {
   if (document.querySelectorAll('[data-range]').length) UI_Control.range.init();
   if (document.querySelectorAll('[data-checkbox]').length) UI_Control.checkAll.init();
   if (document.querySelectorAll('[data-context]').length) UI_Control.Tooltip.init();
+  UI_Control.swiper.init();
 })
