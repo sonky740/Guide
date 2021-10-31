cda67864aec01e02c0ac5dacbc82d1dd47705e90
sonky740 <mykiller4@naver.com>
2021-11-01 08:03:18 +0900

    ios15 test

--- src/js/common.js
+++ src/js/common.js
@@ -124,7 +124,7 @@ UI_Control.layout = {
     })
 
     menuTarget.addEventListener('transitionend', function (e) {
-      if (e.target === menuTarget) {
+      if (e.target === menuTarget && menuClose.classList.contains('trigger')) {
         menuTarget.removeAttribute('style');
         menuTarget.classList.remove('on');
         menuTrigger.classList.add('trigger');
